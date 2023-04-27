15e863ce1337d537ced5ea63837063c145f01523
Jules <juliaghany@msn.com>
2023-04-27 14:24:51 -0500

    remove format_time function because it is not needed for this application

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,6 @@
 // borrowed from Module 14 Activity 15 
 
 module.exports = {
-    format_time: (date) => {
-        return date.toLocaleTimeString();
-    },
     format_date: (date) => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()
             }`;

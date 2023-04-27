7f63f33e19d9fc5d08d44213bc1a3749f6db91e3
Jules <juliaghany@msn.com>
2023-04-27 12:11:08 -0500

    begin working on server.js file

---
+++ server.js
@@ -0,0 +1,46 @@
+// referenced Module 14 Activity 17 server.js file
+
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = ('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up sessions with cookies
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        // expires after 1 hour
+        maxAge: 3600000,
+    },
+    resave: false,
+    saveUninitialied: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => 
+    console.log(`Server running on port ${PORT}`))
+})

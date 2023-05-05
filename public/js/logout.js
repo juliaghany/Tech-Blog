2a0b80610641db87fb2d2d5579a1a609759eda22
Jules <juliaghany@msn.com>
2023-05-05 12:30:59 -0500

    create logout logic

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,3 +1,15 @@
 // refer to Module 14 Activity 17
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
 
-// log out logic here 
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out.')
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);

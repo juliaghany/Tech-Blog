1ba2889fa4b1b04e6233f0f7cf2ded56af022e1b
Jules <juliaghany@msn.com>
2023-05-05 12:18:22 -0500

    specify routes for each model

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+// reference Module 14 Activity 17 controllers -> api -> index.js
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const blogRoutes = require('./blog-routes');
+// const commentsRoutes = require('./comments-routes');
+
+router.use('/users', userRoutes);
+router.use('/blogs', blogRoutes);
+// router.use('/comments', commentsRoutes);
+
+module.exports = router;

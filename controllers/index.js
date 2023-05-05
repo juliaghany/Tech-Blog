10fde441a42febd4e26a81abc22479eef3c735ed
Jules <juliaghany@msn.com>
2023-05-05 12:22:43 -0500

    refactor routing for application by splitting into home and api routes

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+// referenced Module 14 Activity 17 controllers -> index.js
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;

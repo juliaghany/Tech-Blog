fbf78e95e21042685fa9685bf98f3ba998376afe
Jules <juliaghany@msn.com>
2023-05-05 12:30:19 -0500

    create login logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,2 +1,47 @@
-// refer to Module 14 Activity 17
+// refer to Module 14 Activity 17 public -> js -> login.js
+console.log("file linked")
+const loginFormHandler = async (event) => {
+    event.preventDefault();
 
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+    console.log("signing up")
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);

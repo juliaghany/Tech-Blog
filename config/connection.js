aa7cccc6035e26f838a424fd751a5b6c36ce3681
Jules <juliaghany@msn.com>
2023-05-08 17:39:55 -0500

    Pushing to Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,16 @@
 // set up connection to MySQL database using sequelize
 // import dotenv library to load environment variables from .env file 
 
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new sequelize(
+    sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PW,
+        process.env.DB_PASSWORD,
         {
             host: 'localhost',
             dialect: 'mysql',
